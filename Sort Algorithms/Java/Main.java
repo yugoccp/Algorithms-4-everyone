e4872b8d69f203fde9c2f7b1d13f4a21c3ea18dc
Alekos Filini <alekos.filini@gmail.com>
2017-10-01 22:03:12 +0200

    Add the Java implementation of the SelectionSort
    Signed-off-by: Alekos Filini <alekos.filini@gmail.com>

--- Sort Algorithms/Java/Main.java
+++ Sort Algorithms/Java/Main.java
@@ -2,9 +2,9 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        SortAlgorithm<Integer> bubbleSort = new BubbleSort<>();
+        SortAlgorithm<Integer> selectionSort = new SelectionSort<>();
         Integer[] array = new Integer[]{3, 5, 6, 8, 7};
-        bubbleSort.sort(array);
+        selectionSort.sort(array);
 
         ArrayList<Integer> arrayList = new ArrayList<>();
         arrayList.add(3);
@@ -13,7 +13,7 @@ public class Main {
         arrayList.add(8);
         arrayList.add(7);
 
-        bubbleSort.sort(arrayList);
+        selectionSort.sort(arrayList);
 
         for (Integer i : array) {
             System.out.println(i);
