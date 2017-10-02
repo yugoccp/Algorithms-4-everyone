b1445b40c6cf08ac509db9c6752d79a75ae49818
Mehul Singhal <mehulbtp@gmail.com>
2017-10-02 21:19:26 +0530

    Adding the merge sort code for python.

---
+++ Sort Algorithms/Python/Merge_Sort.py
@@ -0,0 +1,31 @@
+# Code for the merge subroutine
+
+def merge(a,b):
+    """ Function to merge two arrays """
+     
+    c = []
+    while len(a) != 0 and len(b) != 0:
+        if a[0] < b[0]:
+            c.append(a[0])
+            a.remove(a[0])
+        else:
+            c.append(b[0])
+            b.remove(b[0])
+    if len(a) == 0:
+        c += b
+    else:
+        c += a
+    return c
+
+# Code for merge sort
+# Recurssive function for merge sort.
+def mergesort(x):
+    """ Function to sort an array using merge sort algorithm """
+    if len(x) == 0 or len(x) == 1:
+        return x
+    else:
+        middle = len(x)/2
+        #We start from middle so that finally the two portions can be merged by after the sorting finishes.
+        a = mergesort(x[:middle])
+        b = mergesort(x[middle:])
+        return merge(a,b)
