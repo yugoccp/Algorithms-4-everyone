5df6d3c68108f029476577347729862b28a5aee0
haydenty <htwade51@gmail.com>
2017-10-02 21:04:05 -0500

    added selectionSortAscending

---
+++ Sort Algorithms/JavaScript/selectionSortAscending.js
@@ -0,0 +1,25 @@
+//after each iternation you have 1 number in the correct positon
+//smallest to largest
+//O(n2) - worst case
+function selectionSortAscending(inputArray){
+    for(var i = 0; i < inputArray.length - 1; i++){
+      var minIndex = i;
+      for(var j = i + 1; j < inputArray.length; j++){
+        if(inputArray[minIndex] > inputArray[j]){
+          minIndex = j;//found new minimum
+        }
+      }
+
+      //swap
+      if(minIndex != i){
+        var temp = inputArray[minIndex];
+        inputArray[minIndex] = inputArray[i];
+        inputArray[i] = temp;
+      }
+      console.log('In progress: ', inputArray);
+    }
+    return inputArray;
+}
+
+var input = [3,2,43,6,777,83,5,5];
+console.log('Output: ', selectionSortAscending(input));
