7570cd95ed7ad0ff1846b2812a67d2407ec70780
khokharnikunj8 <khokharnikunj8@gmail.com>
2017-10-09 19:22:01 +0530

    Add Searching Algorithms Java

---
+++ Search Algorithm/Java/Linear_Search.java
@@ -0,0 +1,18 @@
+/**
+ * @author Nikunj Khokhar
+ */
+public class Linear_Search {
+    public static int Linear_Search(int a[], int b) {
+        //if element b found return position else return -1
+        int len = a.length;
+        for (int i = 0; i < len; i++) {
+            if (a[i] == b) return i;
+        }
+        return -1;
+    }
+
+    public static void main(String as[]) {
+        int a[] = {1, 2, 3, 4, 9, 51, 54, 65, 45, 77};
+        System.out.println(Linear_Search(a, 54));
+    }
+}
