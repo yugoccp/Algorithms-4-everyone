28f9a3e47d2163dc28d8c1f8ed1e63a711e39a27
Daniela Brozzoni <brozzoni@live.it>
2017-10-22 18:22:37 +0200

    Order all the files

---
+++ Algorithms/Graph Algorithms/Topological Sort/Java/TopologicalSort.java
@@ -0,0 +1,40 @@
+
+public class TopologicalSort {
+	int N = 6;
+	public static void main(String args[]){
+		System.out.prinln("Hello world");
+	}
+}
+/*
+vector<int> topologicalOrder;
+vector<int> connected[N];
+bool visited[N];
+
+void dfs_top(int n)
+{
+	visited[n] = 1;
+	for(int i: connected[n])
+		if(!visited[i])
+			dfs_top(i);
+	topologicalOrder.push_back(n);
+}
+
+int main()
+{
+	connected[0].push_back(1);
+	connected[0].push_back(2);
+	connected[1].push_back(2);
+	connected[1].push_back(3);
+	connected[2].push_back(3);
+	connected[2].push_back(5);
+	connected[3].push_back(4);
+
+	fill(visited, visited + N, 0);
+
+	dfs_top(0);
+
+	reverse(topologicalOrder.begin(), topologicalOrder.end());
+
+	for(int i: topologicalOrder)
+		printf("%d ", i);
+}*/
