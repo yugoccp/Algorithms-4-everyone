b6446249e9848e5cd301caad9b577c95112025c4
Daniela Brozzoni <brozzoni@live.it>
2017-10-25 18:56:46 +0200

    Fixed Issues

--- Algorithms/Graph Algorithms/Shortest Path Algorithm/Floyd Warshall/Java/Floyd_Warshall.java
+++ Algorithms/Graph Algorithms/Shortest Path Algorithm/Floyd Warshall/Java/Floyd_Warshall.java
@@ -85,15 +85,19 @@ class Floyd_Warshall
     // Driver method
     public static void main (String[] args)
     {
-        /* Let us create the following weighted graph
+      /* Let us create the following weighted graph
+       
            10
         (0)------->(3)
         |         /|\
-        5 |          |
+      5 |          |
         |          | 1
-        \|/         |
-        (1)------->(2)
-           3           */
+       \|/         |
+       (1)------->(2)
+           3           
+
+      */
+       
         int graph[][] = { {0,   5,  INF, 10},
                           {INF, 0,   3, INF},
                           {INF, INF, 0,   1},
